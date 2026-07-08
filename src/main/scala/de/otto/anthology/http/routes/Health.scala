2faca20659d57e15f5bae80ba51ead309723161c
Karl F Walkow <38581665+dieproht@users.noreply.github.com>
2026-07-08 10:32:47 +0200

    feat(#5): Health endpoint finishing (#20)

--- src/main/scala/de/otto/anthology/http/routes/Health.scala
+++ src/main/scala/de/otto/anthology/http/routes/Health.scala
@@ -4,8 +4,10 @@ import de.otto.anthology.http.Route
 import sttp.tapir.*
 
 object Health:
-    val route: Route = endpoint.get
-        .in("health")
-        .out(stringBody)
-        .handleSuccess(_ => "anthology is healthy")
-        .description("Anthology health check endpoint")
+    val route: Route =
+        endpoint //
+            .get
+            .in("health")
+            .out(stringBody)
+            .handleSuccess(_ => "Anthology is up and running")
+            .description("Anthology Health Check Endpoint")
