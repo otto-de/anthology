3bb49763f3b3931b62a506680ed60fcbe85953cc
Karl F Walkow <KarlFriedrich.Walkow@otto.de>
2026-07-15 15:13:50 +0200

    feat: Make throughput logging more compact

--- src/main/scala/de/otto/anthology/SimpleLoggingCounterStage.scala
+++ src/main/scala/de/otto/anthology/SimpleLoggingCounterStage.scala
@@ -11,30 +11,18 @@ import scala.concurrent.duration.*
 
 object SimpleLoggingCounterStage extends LazyLogging:
 
-    private val hourFactor: Int = 60
-    private val dayFactor: Int = hourFactor * 24
-
     extension [A](source: Flow[A])
         def count(name: String)(using Ox): Flow[A] =
             var currentMinute: Int = 0
             var currentCount: Long = 0
             var currentMinuteStartCount: Long = 0
-            var currentHourStartCount: Long = 0
-            var currentDayStartCount: Long = 0
 
             forkDiscard:
                 repeat(Schedule.fixedInterval(1.minute)):
                     currentMinute = currentMinute + 1
-                    val report = StringBuilder(s"Throughputs at $name \n")
-                    report.append(s"   - Last minute: ${currentCount - currentMinuteStartCount} \n")
+                    val report = s"Throughput at $name: ${currentCount - currentMinuteStartCount} messages per minute"
                     currentMinuteStartCount = currentCount
-                    if currentMinute % hourFactor == 0 then
-                        report.append(s"   - Last hour:   ${currentCount - currentHourStartCount} \n")
-                        currentHourStartCount = currentCount
-                    if currentMinute % dayFactor == 0 then
-                        report.append(s"   - Last day:    ${currentCount - currentDayStartCount} \n")
-                        currentDayStartCount = currentCount
-                    logger.info(report.toString)
+                    logger.info(report)
 
             source.tap: _ =>
                 currentCount = currentCount + 1
