015d94ea6aa70243f6f226302c38b3c1d7e3bdcc
Karl F Walkow <KarlFriedrich.Walkow@otto.de>
2026-07-17 13:07:25 +0200

    feat: Make throughput logging configurable

--- src/main/scala/de/otto/anthology/config/DomainConfig.scala
+++ src/main/scala/de/otto/anthology/config/DomainConfig.scala
@@ -3,5 +3,6 @@ package de.otto.anthology.config
 import de.otto.anthology.ChannelName
 import pureconfig.ConfigReader
 
-case class DomainConfig(channels: Seq[ChannelConfig], relations: Seq[RelationConfig]) derives ConfigReader:
+case class DomainConfig(channels: Seq[ChannelConfig], relations: Seq[RelationConfig], logThroughput: Option[Boolean])
+    derives ConfigReader:
     val channelsByName: Map[ChannelName, ChannelConfig] = channels.map(c => (c.name, c)).toMap
