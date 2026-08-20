0e5346388c0e97dbced7b231a774998b721335c4
Karl F Walkow <KarlFriedrich.Walkow@otto.de>
2026-08-20 14:58:20 +0200

    feat: Log processing times

---
+++ src/main/scala/de/otto/anthology/SimpleProcessingTimeLogger.scala
@@ -0,0 +1,29 @@
+package de.otto.anthology
+
+import com.typesafe.scalalogging.LazyLogging
+
+import java.time.Duration
+import java.time.Instant
+import scala.collection.concurrent.TrieMap
+
+object SimpleProcessingTimeLogger extends LazyLogging:
+
+    private val reportEveryNMessages: Int = 1000
+
+    private val measurements: TrieMap[String, Measurement] = TrieMap.empty
+
+    def measure[T](f: => T)(label: String): T =
+        val start = Instant.now()
+        val result = f
+        val duration = Duration.between(start, Instant.now()).toMillis
+        synchronized:
+            val last = measurements.getOrElse(label, Measurement())
+            val next = Measurement(last.totalDuration + duration, last.count + 1)
+            if next.count % reportEveryNMessages == 0 then
+                val avgMillis = next.totalDuration.toDouble / next.count
+                logger.info(s"$label: average processing time: ${avgMillis}ms")
+                measurements.remove(label)
+            else measurements.update(label, next)
+        result
+
+    private case class Measurement(totalDuration: Long = 0, count: Long = 0)
